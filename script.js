85f67d1f799ef880505fbd4c063127e5f55e3567
psy-a <psyfi488@gmail.com>
2021-04-28 18:00:01 +0300

    Creat Grocery objects.

---
+++ script.js
@@ -0,0 +1,12 @@
+function Grocery (name, price) {
+    this.name = name;
+    this.price = price;
+}
+let Grocery1 = new Grocery("Apple", "40");
+let Grocery2 = new Grocery("Black Olives 100g", "250");
+let Grocery3 = new Grocery("Mango", "40");
+let Grocery4 = new Grocery("Sukuma Wiki", "20");
+let Grocery5 = new Grocery("Pears", "20");
+let Grocery6 = new Grocery("Carrots 50g", "50");
+let Grocery7 = new Grocery("Tomato", "15");
+let Grocery8 = new Grocery("Broccoli", "50");
